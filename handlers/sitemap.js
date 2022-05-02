a92a05da6ea7257ad4779dc11469ac04ad205464
Madhu Babu <17622671+Pandimadhubabu@users.noreply.github.com>
2022-05-02 23:04:08 +0530

    Update sitemap.js

--- handlers/sitemap.js
+++ handlers/sitemap.js
@@ -1,7 +1,7 @@
 import builder from 'xmlbuilder';
 
 async function sitemapHandler() {
-  const base = new URL('https://chickar.ee/');
+  const base = new URL('https://cdn.757live.workers.dev/');
   let urls = [
     base.toString(),
     (new URL('/search', base)).toString(),
